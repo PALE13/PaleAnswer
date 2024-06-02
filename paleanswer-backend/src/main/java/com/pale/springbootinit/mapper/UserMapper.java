4fa12b5c7bad46e5d35cf39f51dd6005a0218592
PALE13 <palezhou13@163.com>
2024-06-03 01:27:29 +0800

    后端项目模板初始化

---
+++ paleanswer-backend/src/main/java/com/pale/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.pale.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pale.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author <a href="https://github.com/PALE13">pale</a>
+ * 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+

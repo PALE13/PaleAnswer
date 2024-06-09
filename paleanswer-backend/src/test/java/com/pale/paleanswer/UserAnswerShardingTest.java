4ae4c0d23a58708a058f7d3f70a8de0c3c527814
PALE13 <palezhou13@163.com>
2024-06-09 18:22:24 +0800

    添加Sharding-JDBC对user_answer分表存储

---
+++ paleanswer-backend/src/test/java/com/pale/paleanswer/UserAnswerShardingTest.java
@@ -0,0 +1,46 @@
+package com.pale.paleanswer;
+
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.pale.paleanswer.mapper.UserAnswerMapper;
+import com.pale.paleanswer.model.entity.UserAnswer;
+import com.pale.paleanswer.service.UserAnswerService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+public class UserAnswerShardingTest {
+
+    @Resource
+    private UserAnswerService userAnswerService;
+
+    @Resource
+    private UserAnswerMapper userAnswerMapper;
+
+    @Test
+    void test() {
+
+//        UserAnswer userAnswer1 = new UserAnswer();
+//        userAnswer1.setAppId(1L);
+//        userAnswer1.setUserId(1L);
+//        userAnswer1.setChoices("1");
+//        userAnswerService.save(userAnswer1);
+//
+//        UserAnswer userAnswer2 = new UserAnswer();
+//        userAnswer2.setAppId(2L);
+//        userAnswer2.setUserId(1L);
+//        userAnswer2.setChoices("2");
+//        userAnswerService.save(userAnswer2);
+//
+//        UserAnswer userAnswerOne = userAnswerService.getOne(Wrappers.lambdaQuery(UserAnswer.class).eq(UserAnswer::getAppId, 1L));
+//        System.out.println(JSONUtil.toJsonStr(userAnswerOne));
+//
+//        UserAnswer userAnswerTwo = userAnswerService.getOne(Wrappers.lambdaQuery(UserAnswer.class).eq(UserAnswer::getAppId, 2L));
+//        System.out.println(JSONUtil.toJsonStr(userAnswerTwo));
+
+
+    }
+}
